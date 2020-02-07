7c81220b2db64c277139098303902b0329b97b7f
Yvonne K <yvonneking017@gmail.com>
2020-02-07 15:11:58 -0600

    Commented out last lines of code

--- 3.3_tables_exercises.sql
+++ 3.3_tables_exercises.sql
@@ -11,12 +11,12 @@ DESCRIBE `departments`;
 # 8. date type employees, dept_emp_latest date, employees, 
 # 9. Each employee would be assigned a department that they work in
 #10 CREATE TABLE `dept_manager` (
-  `emp_no` int(11) NOT NULL,
-  `dept_no` char(4) NOT NULL,
-  `from_date` date NOT NULL,
-  `to_date` date NOT NULL,
-  PRIMARY KEY (`emp_no`,`dept_no`),
-  KEY `dept_no` (`dept_no`),
-  CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE,
-  CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+#  `emp_no` int(11) NOT NULL,
+# `dept_no` CHAR(4) NOT NULL,
+#  `from_date` DATE NOT NULL,
+#  `to_date` DATE NOT NULL,
+#  PRIMARY KEY (`emp_no`,`dept_no`),
+#  KEY `dept_no` (`dept_no`),
+#  CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE,
+# CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE
+# ) ENGINE=INNODB DEFAULT CHARSET=latin1;

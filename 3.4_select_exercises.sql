c73a7fa0f45b2a2d186abcf5824f542b4f66594a
Yvonne K <yvonneking017@gmail.com>
2020-02-07 16:33:25 -0600

    Basic Statements

---
+++ 3.4_select_exercises.sql
@@ -0,0 +1,27 @@
+USE albums_db;
+
+SHOW TABLES;
+
+DESCRIBE albums;
+
+SELECT * FROM albums
+WHERE artist == 'Pink Floyd',
+
+SELECT release_date FROM albums
+WHERE NAME = "Sgt. Pepper's Lonely Hearts Club Band";
+
+SELECT genre FROM albums
+WHERE NAME = 'Nevermind'
+
+SELECT * FROM albums
+WHERE release_date BETWEEN 1989
+AND 2000
+
+SELECT * FROM albums
+WHERE sales < 20
+
+
+SELECT * FROM albums
+WHERE genre = "Rock"
+
+#its only looking for the exact term rock, we would need to use a different operator to find genre's containg rock

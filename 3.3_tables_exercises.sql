a94612f1d96bdb5e03369b655b83e1dccbaeefb6
Yvonne K <yvonneking017@gmail.com>
2020-02-09 14:22:32 -0600

    tweaked and finshed codes

--- 3.3_tables_exercises.sql
+++ 3.3_tables_exercises.sql
@@ -4,19 +4,13 @@ SHOW TABLES;
 
 DESCRIBE employees;
 
-DESCRIBE `departments`;
+#5 Types usesd in employees tabe - int, date, varchar, enum. emp-o uses as Primary key
+
+DESCRIBE departments;
+
+SHOW CREATE TABLE dept_manager;
 
 # 6. numeric - not departments
 # 7. string - all
 # 8. date type employees, dept_emp_latest date, employees, 
-# 9. Each employee would be assigned a department that they work in
-#10 CREATE TABLE `dept_manager` (
-#  `emp_no` int(11) NOT NULL,
-# `dept_no` CHAR(4) NOT NULL,
-#  `from_date` DATE NOT NULL,
-#  `to_date` DATE NOT NULL,
-#  PRIMARY KEY (`emp_no`,`dept_no`),
-#  KEY `dept_no` (`dept_no`),
-#  CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE,
-# CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE
-# ) ENGINE=INNODB DEFAULT CHARSET=latin1;
+# 9. Each employee would be assigned a department that they work in, but this relatioonship would be in the table dept_emp
